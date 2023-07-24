0c49a81212bb7c22cc1dd22e178a2daee90465b3
Jaeha Lee <wogkdkrm112@gmail.com>
2023-07-24 21:31:08 +0900

    init: re-initialize client

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  const sendMessage = () => {};
+
+  return (
+    <div>
+      <input placeholder="message..." />
+      <button onClick={sendMessage}>Send Message</button>
+    </div>
+  );
+}
+
+export default App;
