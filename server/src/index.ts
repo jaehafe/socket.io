f4f334fadb4b25b59a5e8c693594d4e1a46a345f
Jaeha Lee <wogkdkrm112@gmail.com>
2023-07-24 22:17:04 +0900

    feat: [server] joinRoom, sendMessage

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,12 +10,27 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: 'http//localhost:5173',
+    origin: 'http://localhost:5173',
+    credentials: true,
     methods: ['GET', 'POST'],
   },
 });
 
 let port = 3000;
+
+io.on('connection', (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on('join_room', (data) => {
+    console.log('data>>', data);
+
+    socket.join(data);
+  });
+
+  socket.on('send_message', (data) => {
+    socket.to(data.room).emit('receive_message', data);
+  });
+});
 
 server.listen(port, () => {
   console.log(`Server is running ${port}`);
