014526997e56a4f7895b483b83919bf8ab27e98c
Jaeha Lee <wogkdkrm112@gmail.com>
2023-07-24 22:16:36 +0900

    feat: [client] joinRoom, sendMessage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,62 @@
+import { useEffect, useState } from 'react';
+
+import { io, Socket } from 'socket.io-client';
+
+const socket: Socket = io('http://localhost:3000');
+console.log('socket>>>', socket);
+
+socket.on('connect', () => {
+  console.log('connection server');
+});
+
 function App() {
-  const sendMessage = () => {};
+  const [room, setRoom] = useState<string | null>(null);
+  const [message, setMessage] = useState<string | null>(null);
+  const [messageReceived, setMessageReceived] = useState<string | null>(null);
+
+  const joinRoom = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (room !== '') {
+      socket.emit('join_room', room);
+    }
+  };
+
+  const sendMessage = (e: React.FormEvent) => {
+    e.preventDefault();
+    socket.emit('send_message', { message, room });
+  };
+
+  useEffect(() => {
+    socket.on('receive_message', (data) => {
+      console.log('data>>>', data);
+
+      setMessageReceived(data.message);
+    });
+  }, [socket]);
 
   return (
     <div>
-      <input placeholder="message..." />
-      <button onClick={sendMessage}>Send Message</button>
+      <form onSubmit={joinRoom}>
+        <input
+          placeholder="Room Number..."
+          onChange={(e) => {
+            setRoom(e.target.value);
+          }}
+        />
+        <button type="submit"> Join Room</button>
+      </form>
+
+      <form onSubmit={sendMessage}>
+        <input
+          placeholder="Message..."
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+        />
+        <button type="submit">Send Message</button>
+      </form>
+      <h1> Message:</h1>
+      {messageReceived}
     </div>
   );
 }
